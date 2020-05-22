6fc7ce7bb109d4cd3d44c920a865c1238d40ba14
Bismarck6502 <sam@aepearce.com>
2020-05-22 15:38:31 +0200

    Created base with spring initializr

---
+++ src/test/java/ch/pearcenet/eventapi/EventapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.pearcenet.eventapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
